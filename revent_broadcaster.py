72598131b549122eb664d6b31e258ebe81977f39
Robby Ranshous <rranshous@gmail.com>
2012-03-04 18:12:08 -0500

    working toward getting pusher working

--- revent_broadcaster.py
+++ revent_broadcaster.py
@@ -8,19 +8,21 @@ from producer import Producer
 
 NS = 'revent_broadcaster'
 
+class SubscriptionHandler(Thread):
 
-class SubscriptionHandler:
-    """
-    watches revent event queue for given subscriber
-    and pushes events to their end point
-    """
-
-    event_verify_timeout = 3 * 60 # 3min
     timeout = 60
-    retry_frequency = 60 * 5 # = 5min
+    # after receiving a bad response from the client
+    # how long do we wait before trying the event agian
+    retry_frequency = 60 * 1 # = 1 min
+    # how long should we sleep if the client
+    # throws an error ?
+    fail_sleep_time = 60.0 * .5 # 30 seconds
 
     def __init__(self, is_stopping,
                        subscription_key, consumer_url, domain):
+
+        # respect
+        Thread.__init__(self)
 
         # flag whether we are shutting down
         self.is_stopping = is_stopping
@@ -32,47 +34,8 @@ class SubscriptionHandler:
         self.producer = Producer(self.consumer_url,
                                  self.domain)
 
-        # how long should we sleep if the client
-        # throws an error ?
-        self.fail_sleep_time = 60 * 1 # 1 min
 
-        # flag whether we should be removed from pool
-        self.dead = False
-
-        # get the details of our subscription
-        self.watched_events = []
-        self.update_subscription_details()
-
-        # setup our revent client, we want msgs which
-        # we don't mark handled to be re-introduced to
-        # the queue.
-        # channel key is subscription key, auto create channel
-        self.revent = ReventClient(subscription_key,
-                                   self.revent_filter_string,
-                                   True, True,
-                                   self.event_verify_timeout)
-
-    def update_subscription_details(self):
-        """
-        hits redis to get our details
-        """
-        key = '%s:subscription:%s:watched_events' % (NS,self.subscription_key)
-        self.watched_events = rc.lrange(key,0,-1)
-
-    def get_revent_filter_string(self):
-        """
-        builds a revent regex string for event names
-        based on the watched events for this subscription
-        """
-
-        # the regex is going to be OR'd together event names
-        regex_string = '|'.join(self.watched_events)
-        return regex_string
-
-    # setup the filter string as a property
-    revent_filter_string = property(get_revent_filter_string)
-
-    def start(self):
+    def run(self):
         """
         sit on the revent queue for the subscription
         passing events as HTTP(S) requests
@@ -82,17 +45,16 @@ class SubscriptionHandler:
         while not self.is_stopping.is_set():
 
             # try and get an event
-            event_name, event_data = revent.get_event(timeout=self.timeout)
+            event_data = self.get_event()
 
             # if didn't get one just go back around
-            if event_name is None:
+            if event_data is None:
                 continue
 
             # we got an event, broadcast it to our consumer
             status_code = None
             try:
-                status_code = self.producer.send_event(data=event_data,
-                                                       name=event_name)
+                status_code = self.producer.send_event(data=event_data)
             except Exception, ex:
                 # woops, the event will be requeued
                 print 'exception sending event: %s' % ex
@@ -127,22 +89,164 @@ class SubscriptionHandler:
                 self.cancel_retry(event_name, event_data)
                 self.fail_sleep()
 
+
+    def update_subscription_details(self):
+        """
+        updates our details from our shared storage
+        """
+        raise NotImplementedError
+
+    def get_event(self):
+        """
+        get the next event from the queue
+        """
+        raise NotImplementedError
+
     def remove_subscription(self):
         """
-        removes our subscription for events, marks self as dead
+        Updates details for subscription on shared storage
+        as being done. Also clears outstanding events,
+        broadcasts that our subscription's state has changed
         """
+
+        # remove our subscription details
+        self.remove_subscription_details()
+
+        # let the other broadcaster's know our details have changed
+        self.broadcast_state_change()
+
+    def remove_subscription_details(self):
+        """
+        removes our details from shared storage
+        """
+        raise NotImplementedError
+
+    def broadcast_state_change(self):
+        """
+        broadcasts on redis pubsub that our subscription details have changed
+        """
+        raise NotImplementedError
+
+    def verify_event_processed(self, event_name, event_data):
+        """
+        notifies that we have finished processing the event and it
+        should not be requeued
+        """
+        raise NotImplementedError
+
+    def cancel_retry(self, event_name, event_data):
+        """
+        makes sure the event we are currently handling does
+        not get requeued
+        """
+        raise NotImplementedError
+
+    def fail_sleep(self):
+        """
+        after failing to make a request we want
+        to sleep for a bit
+        """
+
+        if not self.is_stopping.is_set():
+            sleep(self.fail_sleep_time)
+
+        # make the fail sleep time longer so that if we fail again
+        # the next attempt will be later
+        self.fail_sleep_time *= 2
+
+        return True
+
+class ReventSubscriptionHandler(SubscriptionHandler):
+    """
+    watches revent event queue for given subscriber
+    and pushes events to their end point
+    """
+
+    event_verify_timeout = 3 * 60 # 3min
+
+    def __init__(self, *args):
+
+
+        # get the details of our subscription
+        self.watched_events = []
+        self.update_subscription_details()
+
+        # redis client
+        self.rc = None
+
+        # setup our revent client, we want msgs which
+        # we don't mark handled to be re-introduced to
+        # the queue.
+        # channel key is subscription key, auto create channel
+        self.revent = ReventClient(subscription_key,
+                                   self.revent_filter_string,
+                                   True, True,
+                                   self.event_verify_timeout)
+
+
+    def run(self):
+        self.rc = redis.Redis(REDIS_HOST)
+        SubscriptionHandler.run(self)
+
+    def update_subscription_details(self):
+        """
+        hits redis to get our details
+        """
+        key = '%s:subscription:%s:watched_events' % (NS,self.subscription_key)
+        self.watched_events = rc.lrange(key,0,-1)
+
+    def get_revent_filter_string(self):
+        """
+        builds a revent regex string for event names
+        based on the watched events for this subscription
+        """
+
+        # the regex is going to be OR'd together event names
+        regex_string = '|'.join(self.watched_events)
+        return regex_string
+
+    # setup the filter string as a property
+    revent_filter_string = property(get_revent_filter_string)
+
+    def get_event(self):
+        """
+        attemps to get the next event, blocking.
+        returns event_data or None
+        """
+        event_name, event_data = revent.get_event(block=True,
+                                                  timeout=self.timeout)
+
+        # add the name to the event data if it's not present
+        if not '_name' in event_data:
+            event_data['_name'] = event_name
+
+        return event_data
+
+    def remove_subscription(self):
+        """
+        removes our subscription for events, broadcasts that
+        our subscription state has changed
+        """
+
+        # respect
+        SubscriptionHandler.remove_subscription(self)
 
         # tell revent to remove our event channel
         revent.remove_channel()
+
+    def remove_subscription_details(self):
 
         # remove our subscription details from redis
         key = '%s:subscription:%s:watched_events' % (NS,self.subscription_key)
         rc.del(key)
 
-        # let the other broadcaster's know our details have changed
-        self.server.broadcast_subscriber_change(self.subscription_key)
+    def broadcast_state_change(self):
+        """
+        broadcasts on redis pubsub that our subscription details have changed
+        """
 
-        return True
+        rc.publish('%s:subscription_change' % NS,
+                   self.subscription_key)
 
     def verify_event_processed(self, event_name, event_data):
         """
@@ -160,21 +264,6 @@ class SubscriptionHandler:
         # to cancel a retry we'll just mark the event handled
         return self.verify_event_processed(event_name, event_data)
 
-    def fail_sleep(self):
-        """
-        after failing to make a request we want
-        to sleep for a bit
-        """
-
-        if not self.is_stopping.is_set():
-            sleep(self.fail_sleep_time)
-
-        # make the fail sleep time longer so that if we fail again
-        # the next attempt will be later
-        self.fail_sleep_time *= 2
-
-        return True
-
 
 class Broadcaster:
     """
@@ -190,16 +279,53 @@ class Broadcaster:
     # a thread which is going to wait for broadcasts about
     #  subscriber changes
 
+    sleep_time = 2
+
+    SubscriptionChangeListener = SubscriptionChangeListener
+
     def __init__(self, domain='defaultdomain'):
 
         # what domain are the events broadcast from?
         self.domain = domain
 
-        # lookup of subscriber keys
+        # lookup of subscriber threads
         self.subscriber_lookup = {}
 
         # flag so that we can co-ordinate threads stopping
-        self.is_stopping =
+        self.is_stopping = Event()
+
+        # our queue for alerting threads to subscription change
+        self.subscription_change_queue = Queue()
+
+        # our thread which listenes for broadcasts alerting
+        # us to subscriber detail changes
+        self.subscription_change_thread = None
+
+    def start_subscription_change_listener(self):
+        """
+        starts a thread which will listen for broadcasts
+        alerting us to subscriber changes
+        """
+
+        try:
+
+            self.subscription_change_thread = \
+                    self.SubscriptionChangeListener(
+                        self.is_stopping,
+                        self.subscription_change_queue)
+
+            self.subscription_change_thread.start()
+
+        except Exception, ex:
+            print 'Exception starting change listener thread: %s' % ex
+            self.is_stopping.set()
+
+
+    def sync_subscription_handlers(self):
+        """
+        bring our subscription handlers in to line w/ the
+        shared data store
+        """
 
 
     def run(self):
@@ -214,3 +340,59 @@ class Broadcaster:
         self.sync_subscription_handlers()
 
         # now put the master thread in a loop
+        while not self.is_stopping.is_set():
+            sleep(self.sleep_time)
+
+
+
+class SubscriptionChangeListener(Thread):
+
+    """
+    listens for subscription change broadcasts,
+    puts the details on the change queue
+    """
+
+    def __init__(self, is_stopping, change_queue):
+        self.change_queue = change_queue
+        self.is_stopping = is_stopping
+        Thread.__init__(self)
+
+    def run(self):
+        """
+        blocks, listening to subscription change broadcasts
+        and adding change details to queue
+        """
+
+        while not self.is_stopping.is_set():
+
+            # _listen method will block until it gets a msg
+            for msg_data in self._listen_subscription_change():
+
+                # get the subscription key from the msg
+                subscription_key = msg_data.get('data')
+
+                # put the details on our queue
+                self.change_queue.push(subscription_key)
+
+    def _listen_subscription_change(self):
+        # do this yourself
+        raise NotImplementedError
+
+class RedisSubscriptionChangeListener(SubscriptionChangeListener):
+
+    def __init__(self, *args, **kwargs):
+        SubscriptionChangeListener.__init__(self,*args,**kwargs)
+
+        # our redis clients
+        self.rc = None
+        self.rc_pubsub = None
+
+    def run(self):
+        self.rc = redis.Redis(REDIS_HOST)
+        self.rc_pubsub = self.rc.pubsub()
+        self.rc_pubsub.subscribe('%s:subscription_change' % NS)
+        SubscriptionChangeListener.run(self)
+
+    def _listen_subscription_change(self):
+        return self.rcps.listen()
+
